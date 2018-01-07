d6e92b30828e94098c6f0825790e49cf2ccab503
Your Name <you@example.com>
2018-01-07 16:02:23 +0800

    predict per 6 hours

--- cosine-similarity.py
+++ cosine-similarity.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd 
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
+from pathlib import Path
 
 def getTTvalue(allTrainingDataset, allTestingDataset):
 	returnData = pd.DataFrame()
@@ -16,151 +17,169 @@ def getTTvalue(allTrainingDataset, allTestingDataset):
 		returnData = returnData.append(allData[allData.index == trainDataIP])
 	return returnData
 
-def mutiplyVector(a, b, weight):
-	temp = 0
-	for i in range(a.size):
-		temp += a[i] * b[i] * weight
-	return temp
+
+""" main """
+if __name__ == "__main__":
+
+	correctMean = 0
+	allFiles = 4
+	dataset1Len = 0
+	dataset2Len = 0
+	for i in range(allFiles):
+		trainingFile = './data/training/6_hour/training_data2_6hour_' + str(i * 6) + '.csv'
+		testingFile = './data/testing/6_hour/testing_data2_6hour_' + str(i * 6) +'.csv'
+
+		trainingFileExist = Path(trainingFile)
+		testingFileExist = Path(testingFile)
+		if(trainingFileExist.exists() & testingFileExist.exists()):
+
+			""" reaed testing  and training file """
+			column_names = ['IP', 'Domain', 'flag', 'date']
+			dataset1 = pd.read_csv(trainingFile)
+			dataset2 = pd.read_csv(testingFile)
+			dataset1Len += len(dataset1)
+			dataset2Len += len(dataset2)
+			dataset1.columns = column_names
+			dataset2.columns = column_names
+			print("1. Read files %s finish." % trainingFile)
+
+			"""
+
+				allDataset:
+				+-------------------+
+				|	training data 	|
+				+-------------------+
+				|	testing  data 	|
+				+-------------------+
+
+			"""
+			allDataset = dataset1.append(dataset2)
+			allTrainingDataset = pd.DataFrame({'count': dataset1.groupby(['IP', 'Domain']).size()}).unstack().reset_index().fillna(0).set_index('IP')
+			allTestingDataset = pd.DataFrame({'count': dataset2.groupby(['IP', 'Domain']).size()}).unstack().reset_index().fillna(0).set_index('IP')
 
 
-trainingFile = 'traing_data2.csv'
-testingFile = 'testing_data2.csv'
+			"""
 
-column_names = ['IP', 'Domain', 'flag', 'date']
-dataset1 = pd.read_csv(trainingFile)
-dataset2 = pd.read_csv(testingFile)
-dataset1.columns = column_names
-dataset2.columns = column_names
+				realTrainX: when in training data time
+				+---------------------------------------------------------------------------------+
+				|      | visit domain 1 times | visit domain 2 times | ... | visit domain N times |
+				|---------------------------------------------------------------------------------|
+				| IP 1 |                      |                      |     |                      |
+				|---------------------------------------------------------------------------------|
+				| IP 2 |                      |                      |     |                      |
+				|---------------------------------------------------------------------------------|
+				|                                        .                                        |	
+				|                                        .                                        |
+				|                                        .                                        |
+				|---------------------------------------------------------------------------------|
+				| IP N |                      |                      |     |                      |
+				+---------------------------------------------------------------------------------+
 
-"""
-
-	allDataset:
-	+-------------------+
-	|	training data 	|
-	+-------------------+
-	|	testing  data 	|
-	+-------------------+
-
-"""
-allDataset = dataset1.append(dataset2)
-
-
-allTrainingDataset = pd.DataFrame({'count': dataset1.groupby(['IP', 'Domain']).size()}).unstack().reset_index().fillna(0).set_index('IP')
-allTestingDataset = pd.DataFrame({'count': dataset2.groupby(['IP', 'Domain']).size()}).unstack().reset_index().fillna(0).set_index('IP')
+			"""
+			realTrainX = getTTvalue(allTrainingDataset, allTestingDataset)
+			print("2.1 Transform trainging data to X finish.")
 
 
-"""
+			"""
 
-	realTrainX: when in training data time
-	+---------------------------------------------------------------------------------+
-	|      | visit domain 1 times | visit domain 2 times | ... | visit domain N times |
-	|---------------------------------------------------------------------------------|
-	| IP 1 |                      |                      |     |                      |
-	|---------------------------------------------------------------------------------|
-	| IP 2 |                      |                      |     |                      |
-	|---------------------------------------------------------------------------------|
-	|                                        .                                        |	
-	|                                        .                                        |
-	|                                        .                                        |
-	|---------------------------------------------------------------------------------|
-	| IP N |                      |                      |     |                      |
-	+---------------------------------------------------------------------------------+
+				realTestY: when in testing data time
+				+---------------------------------------------------------------------------------+
+				|      | visit domain 1 times | visit domain 2 times | ... | visit domain N times |
+				|---------------------------------------------------------------------------------|
+				| IP 1 |                      |                      |     |                      |
+				|---------------------------------------------------------------------------------|
+				| IP 2 |                      |                      |     |                      |
+				|---------------------------------------------------------------------------------|
+				|                                        .                                        |	
+				|                                        .                                        |
+				|                                        .                                        |
+				|---------------------------------------------------------------------------------|
+				| IP N |                      |                      |     |                      |
+				+---------------------------------------------------------------------------------+
 
-"""
-realTrainX = getTTvalue(allTrainingDataset, allTestingDataset)
-
-
-"""
-
-	realTestY: when in testing data time
-	+---------------------------------------------------------------------------------+
-	|      | visit domain 1 times | visit domain 2 times | ... | visit domain N times |
-	|---------------------------------------------------------------------------------|
-	| IP 1 |                      |                      |     |                      |
-	|---------------------------------------------------------------------------------|
-	| IP 2 |                      |                      |     |                      |
-	|---------------------------------------------------------------------------------|
-	|                                        .                                        |	
-	|                                        .                                        |
-	|                                        .                                        |
-	|---------------------------------------------------------------------------------|
-	| IP N |                      |                      |     |                      |
-	+---------------------------------------------------------------------------------+
-
-"""
-realTestY = getTTvalue(allTestingDataset, allTrainingDataset)
+			"""
+			realTestY = getTTvalue(allTestingDataset, allTrainingDataset)
+			print("2.2 Transform testing data to Y finish.")
 
 
-"""
+			"""
 
-	domainFlag: (type: pandas dataFrame)
-	+----------------------------+
-	|		   | is core domain? |
-	|----------------------------|
-	| damain 1 |                 |
-	|----------------------------|
-	| damain 2 |                 |
-	|----------------------------|
-	|             .              |
-	|             .              |
-	|             .              |
-	|----------------------------|
-	| damain N |                 |
-	+----------------------------+
+				domainFlag: (type: pandas dataFrame)
+				+----------------------------+
+				|		   | is core domain? |
+				|----------------------------|
+				| damain 1 |                 |
+				|----------------------------|
+				| damain 2 |                 |
+				|----------------------------|
+				|             .              |
+				|             .              |
+				|             .              |
+				|----------------------------|
+				| damain N |                 |
+				+----------------------------+
 
-"""
-domainFlag = allDataset.drop(['IP', 'date'])
-domainFlag = domainFlag.drop_duplicates(subset=['Domain'], keep='first')
-domainFlag = domainFlag.reset_index(drop=True)
-domainFlag = domainFlag.set_index('Domain')
-
-
-"""
-	domainWeight: (type: numpy array)
-	+-------------------------------------------------------+
-	|                | domain 1 | domain 2 | ... | domain N |
-	|-------------------------------------------------------|
-	| is core domain |          |          | ... |          |
-	+-------------------------------------------------------+
-
-"""
-domainWeight = np.array([0] * len(realTrainX['count'].columns))
-i = 0
-for domainID in realTrainX['count'].columns:
-	flag = domainFlag.loc[domainID]['flag']
-	if(flag):
-		weight = 1
-	else:
-		weight = 0.1
-	domainWeight[i] =  weight
-	i = i + 1
-
-trainingX = realTrainX[:5].copy()
-testingY = realTestY[:5].copy()
+			"""
+			domainFlag = allDataset.drop(['IP', 'date'])
+			domainFlag = domainFlag.drop_duplicates(subset=['Domain'], keep='first')
+			domainFlag = domainFlag.reset_index(drop=True)
+			domainFlag = domainFlag.set_index('Domain')
 
 
+			"""
+				domainWeight: (type: numpy array)
+				+-------------------------------------------------------+
+				|                | domain 1 | domain 2 | ... | domain N |
+				|-------------------------------------------------------|
+				| is core domain |          |          | ... |          |
+				+-------------------------------------------------------+
 
-correct = 0
-nowIndex = 0
-
-''' do vector inner product '''
-for testDataIP in realTestY.index:
-	print nowIndex, '->', len(realTestY.index)
-	nowIndex = nowIndex + 1
-	b = realTestY.loc[testDataIP].values
-	maxSum = -1
-	maxSumIP = -1
-	tempSum = 0
-	for trainDataIP in realTrainX.index:
-		a = realTrainX.loc[trainDataIP].values
-		tempSum = (a * b * domainWeight).sum()
-		#tempSum = (a * b).sum()
-		if(tempSum > maxSum):
-			maxSum = tempSum
-			maxSumIP = trainDataIP
-	if(testDataIP == maxSumIP):
-		correct = correct + 1
-
-print correct
+			"""
+			domainWeight = np.array([0] * len(realTrainX['count'].columns))
+			i = 0
+			for domainID in realTrainX['count'].columns:
+				flag = domainFlag.loc[domainID]['flag']
+				if(flag):
+					weight = 1
+				else:
+					weight = 1
+				domainWeight[i] =  weight
+				i = i + 1
+			print("3. Calculate domains' weight finish.")
 
 
+			''' for toy test '''
+			# trainingX = realTrainX[:5].copy()
+			# testingY = realTestY[:5].copy()
+
+
+			''' calculating accuracy '''
+			correct = 0
+			nowIndex = 0
+
+			for testDataIP in realTestY.index:
+				# print nowIndex, '->', len(realTestY.index)
+				nowIndex = nowIndex + 1
+				b = realTestY.loc[testDataIP].values
+				maxSum = -1
+				maxSumIP = -1
+				tempSum = 0
+				for trainDataIP in realTrainX.index:
+					a = realTrainX.loc[trainDataIP].values
+					''' do vector inner product '''
+					tempSum = (a * b * domainWeight).sum()
+					if(tempSum > maxSum):
+						maxSum = tempSum
+						maxSumIP = trainDataIP
+				if(testDataIP == maxSumIP):
+					correct = correct + 1
+
+			print("4. Calculate predicting accuracy finish.")
+			print "accuracy:", correct / float(len(realTestY.index))
+			correctMean += correct / float(len(realTestY.index))
+			print(correctMean / float(allFiles)), "\n"
+
+	print("5. all file finish, mean accuracy:")
+	print(correctMean / float(allFiles))
+	print(dataset1Len / float(allFiles))
+	print(dataset2Len / float(allFiles))
